b707ccbb327a8a0571f00b0aa729a0b28469272b
shawn owiro <shawnowiro@gmail.com>
2025-01-30 00:39:35 +0300

    updated next config

--- sedo-logistics/next.config.mjs
+++ sedo-logistics/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {webpack: (config) => {
+    config.externals = { mongoose: 'commonjs mongoose' };
+    return config;
+  },};
 
 export default nextConfig;
