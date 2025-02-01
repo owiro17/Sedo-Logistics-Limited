1db15019f65cb63c031683eb7d88375d94cb4897
shawn owiro <shawnowiro@gmail.com>
2025-02-01 03:07:28 +0300

    created schema and connect db

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
-// import mongoose, { Schema } from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
-// const userSchema = new Schema({
-//   email: { type: String, required: true, unique: true }, // Ensure email is unique
-//   firstName: { type: String },
-//   lastName: { type: String },
-//   createdAt: { type: Date, required: true },
-//   phoneNumber: { type: String, required: false },
-//   companyName: { type: String, required: false },
-//   userid: { type: String, unique: true, sparse: true }, // Sparse index to avoid null conflicts
-// });
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true }, // Ensure email is unique
+  firstName: { type: String },
+  lastName: { type: String },
+  createdAt: { type: Date, required: true },
+  phoneNumber: { type: String, required: false },
+  companyName: { type: String, required: false },
+  userid: { type: String, unique: true, sparse: true }, // Sparse index to avoid null conflicts
+});
 
-// const User = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 
-// export default User;
+export default User;
