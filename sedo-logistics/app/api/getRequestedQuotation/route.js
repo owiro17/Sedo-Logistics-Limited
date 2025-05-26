31c8fe303a6d456e9aff1737dcea824914756e4a
shawn owiro <shawnowiro@gmail.com>
2025-05-26 16:41:26 +0300

    fixed import issues

--- sedo-logistics/app/api/getRequestedQuotation/route.js
+++ sedo-logistics/app/api/getRequestedQuotation/route.js
@@ -6,7 +6,7 @@ import connectToDatabase from '../../../libs/connectDB';
 // import {createOrUpdateUser} from '../../../libs/actions/createUpdateUser';
 import { currentUser} from '@clerk/nextjs/server'
 import User from '../../../models/user';
-import requestedQuotationSchema from '../../../models/requestedQuotesfig';
+import requestedQuotationSchema from '../../../models/requestedQuotes';
 
 export async function POST(Request){
     // Ensure user is authenticated
